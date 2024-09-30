222a7d58179455bce841f173cf70d687d4bd69a0
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-30 16:05:45 +0200

    1

---
+++ src/src/Esercizio1/Main.java
@@ -0,0 +1,34 @@
+package Esercizio1;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    //**************************** parte prima **********************//
+
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        // ho istanziato l'oggetto
+
+        int [] array = new int [5];
+
+        // creo numero random con questo metodo
+
+        Random random = new Random();
+        //  faccio un ciclo per dare all'indice un numero casuale
+        for(int i = 0; i< array.length; i++){
+            // qui mi genero un numero random da 1 a 10
+            int randomNum = random.nextInt(1,10);
+            // metto il numero casuale nell'array
+            array[i]= randomNum;
+        }
+        // uso il metodo degli array per trasformate in sringa
+        System.out.println(Arrays.toString(array));
+        //****************************parte seconda****************************//
+int indice = 10;
+
+
+    }
+}
